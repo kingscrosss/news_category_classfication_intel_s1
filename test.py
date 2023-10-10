321e5ada33b6fffebe9deb6fdc25be6c2ff4403d
kingscrosss <145517821+kingscrosss@users.noreply.github.com>
2023-10-10 14:55:09 +0900

    first commit

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 # 오른쪽 하단(python~ ) 클릭 - add new interpreter - 파이썬 3.7로 변경해주기(지금까지 다 이걸로 진행했으니까)
 # 입력(수정) 다 한 다음 commit 버튼 클릭 - 메모 남기기 - commit 클릭
-# push
+# push (처음 할 때 아이디 및 토큰 입력)
 print('Hello git!')
