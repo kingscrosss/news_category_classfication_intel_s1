22ee109783c9b5b8cd4a5cf8ed6ee1f0816ef36b
kingscrosss <145517821+kingscrosss@users.noreply.github.com>
2023-12-29 09:56:21 +0900

    Backup

--- job06_model_predict.py
+++ job06_model_predict.py
@@ -16,7 +16,7 @@ df.info()
 X = df['titles']
 Y = df['category']
 
-with open('./models/encoder.pickle', 'rb') as f:    # 이번엔 바이너리로 읽어들여오는거(rb)
+with open('../models/encoder.pickle', 'rb') as f:    # 이번엔 바이너리로 읽어들여오는거(rb)
     encoder = pickle.load(f)
 labeled_y = encoder.transform(Y)    # fit.transform 아니고 그냥 transform. 전자는 새로만드는 것이다
 label = encoder.classes_
@@ -28,7 +28,7 @@ okt = Okt()
 
 for i in range(len(X)):
     X[i] = okt.morphs(X[i], stem=True)
-stopwords = pd.read_csv('./datasets/stopwords.csv', index_col=0)
+stopwords = pd.read_csv('datasets/stopwords.csv', index_col=0)
 
 for j in range(len(X)):
     words = []
@@ -38,7 +38,7 @@ for j in range(len(X)):
                 words.append(X[j][i])
     X[j] = ' '.join(words)
 
-with open('./models/news_token.pickle','rb') as f:
+with open('../models/news_token.pickle', 'rb') as f:
     token = pickle.load(f)
 
 tokened_x = token.texts_to_sequences(X)
@@ -48,7 +48,7 @@ for i in range(len(tokened_x)):
         tokened_x[i] = tokened_x[i][:22]
 x_pad = pad_sequences(tokened_x,21)
 
-model = load_model('./models/news_category_classification_model_0.6978723406791687.h5')
+model = load_model('../models/news_category_classification_model_0.6978723406791687.h5')
 preds = model.predict(x_pad)
 predicts = []
 for pred in preds:
